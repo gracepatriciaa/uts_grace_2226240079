ef900f4d56a4e466776e7dcce106a36b85d2cf93
gracepatriciaa <gracepatricia_2226240079@mhs.mdp.ac.id>
2025-04-16 09:03:40 +0700

    uts

--- lib/repositories/gempa_repository.dart
+++ lib/repositories/gempa_repository.dart
@@ -1,19 +1,34 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:mahasiswa/models/gempa_model.dart';
+import '../models/gempa_model.dart';
 
 class GempaRepository {
   final String apiUrl = 'https://data.bmkg.go.id/DataMKG/TEWS/autogempa.json';
 
-  Future<Gempa> fetchGempaTerakhir() async {
-    final response = await http.get(Uri.parse(apiUrl));
+  // Helper untuk URL shakemap
+  String getShakemapUrl(String filename) {
+    if (filename.isEmpty) return '';
+    return 'https://data.bmkg.go.id/DataMKG/TEWS/$filename';
+  }
 
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      final gempaData = jsonData['Infogempa']['gempa'];
-      return Gempa.fromJson(gempaData);
-    } else {
-      throw Exception('Gagal memuat data gempa');
+  Future<Gempa> fetchGempaTerakhir() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final gempaData = jsonData['Infogempa']['gempa'] ?? {};
+
+        // Pastikan field ada
+        return Gempa.fromJson({
+          ...gempaData,
+          'Shakemap': getShakemapUrl(gempaData['Shakemap'] ?? ''),
+        });
+      } else {
+        throw Exception('Gagal memuat data: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error saat mengambil data: $e');
     }
   }
 }
